f7d1b23d942af1ee1514618b4cab00592002d601
renatojobal <renatojobal@gmail.com>
2020-07-30 15:37:20 -0500

    Creatad last four models

--- core/models.py
+++ core/models.py
@@ -30,21 +30,80 @@ class Person(models.Model):
 
 
 class Tour(models.Model):
+    """
+    """
 
-    pass
+    estimated_time = models.IntegerField()
+    max_visitors = models.IntegerField()
 
 
 class Single(models.Model):
+    """
+    """
 
-    pass
+    nickname = models.CharField(max_length=255)
+    age = models.IntegerField()
+    origin = models.CharField(max_length=255)
+    born_date = models.DateField()
+    born_place = models.CharField(max_length=255)
+
+    specie = models.ForeignKey(to=Specie,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
+
+    shelter = models.ForeignKey(to=Shelter,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
 
 
 class Single_Tour(models.Model):
+    """
+    """
 
-    pass  
+    single = models.ForeignKey(to=Single,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
+
+    tour = models.ForeignKey(to=Tour,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
+
+ 
 
 
 class Employee(models.Model):
+    """
+    """
 
-    pass
+    salary = models.DecimalField()
+    time_enter = models.CharField(max_length=100,
+                                   blank=True,
+                                   null=True)
+    time_exit = models.CharField(max_length=100,
+                                    blank=True,
+                                   null=True)
+
+    person = models.ForeignKey(to=Person,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
+
+    rol = models.ForeignKey(to=Rol,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
+
+    shelter = models.ForeignKey(to=Shelter,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT)
     
+
+
+
+
+
