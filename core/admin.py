91387e1f0b0ee248a2fe1c3aeb17bc2740286939
renatojobal <renatojobal@gmail.com>
2020-07-30 15:10:55 -0500

    Create empty tables

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# Importar modelos
+from core.models import (Shelter)
+
+
+# Registrar los modelos
+admin.site.register(Shelter)
+
+
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Shelter(models.Model):
+
+    meters = models.IntegerField()
+    city = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+class Visit(models.Model):
+
+    pass
+
+
+class Specie(models.Model):
+
+    pass
+
+
+class Rol(models.Model):
+
+    pass
+
+
+class Person(models.Model):
+
+    pass
+
+
+class Tour(models.Model):
+
+    pass
+
+
+class Single(models.Model):
+
+    pass
+
+
+class Single_Tour(models.Model):
+
+    pass  
+
+
+class Employee(models.Model):
+
+    pass
+    
